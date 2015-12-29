e1b9be06d1d868db94e7327594811c90436ab2dd
Alexander Shipin <shipin@sibers.com>
2015-12-29 09:53:58 +0600

    Initial Commit

---
+++ BumBom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BumBom
+//
+//  Created by Alexander Shipin on 29/12/15.
+//  Copyright © 2015 Alexander Shipin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
